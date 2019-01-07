a3bb41ffb478e665837d765b176d8e0a49c7c38f
unknown <aghna_zhalila_26rpl@student.smktelkom-mlg.sch.id>
2019-01-08 06:31:28 +0700

    selesai

---
+++ latihan7.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrBuah = array ("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah[0];echo "<br>"; //Mangga
+echo $arrBuah[3];echo "<br>"; //Jeruk
+
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[0];echo "<br>"; //Merah
+echo $arrWarna[2];echo "<br>"; //Hijau
+
+?>
